5692441f3de12c9b03029e2a6aa0a35b988a83f4
shiva-git-cpu <shivaramguguloth24@gmail.com>
2025-09-09 11:37:19 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "Terraform-sample"
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "key_name" {
+  description = "Existing EC2 Key Pair name"
+  type        = string
+  default     = "shiva"
+}
